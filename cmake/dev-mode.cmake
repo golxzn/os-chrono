b754ec9a5ab0e4349a639582ec84a5a187f0c7ba
golxzn <golxzn@gmail.com>
2023-10-22 10:02:30 +0400

    🤯OMG. Fix missssssssspelling

--- cmake/dev-mode.cmake
+++ cmake/dev-mode.cmake
@@ -1,13 +1,13 @@
 
-if(NOT GXZN_GXZN_CHRONO_DEV_MODE)
+if(NOT GXZN_CHRONO_DEV_MODE)
 	return()
 endif()
 
-if(GXZN_GXZN_CHRONO_BUILD_TEST)
+if(GXZN_CHRONO_BUILD_TEST)
 	enable_testing()
-	add_subdirectory(${GXZN_GXZN_CHRONO_TEST_DIR})
+	add_subdirectory(${GXZN_CHRONO_TEST_DIR})
 endif()
 
-if(GXZN_GXZN_CHRONO_GENERATE_DOCS)
-	include(${GXZN_GXZN_CHRONO_ROOT}/cmake/automatics/docs.cmake)
+if(GXZN_CHRONO_GENERATE_DOCS)
+	include(${GXZN_CHRONO_ROOT}/cmake/automatics/docs.cmake)
 endif()
